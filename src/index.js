db93fc29a0701e6ee38c580f1e8870a090ef868c
sinyoungnam <sinnam90@gmail.com>
2018-10-15 15:04:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,59 @@ document.addEventListener('DOMContentLoaded', () => {
   // your solution here
   // grab DOM elements
   const listDiv = document.getElementById("app-content");
+  const createListForm = document.getElementById("create-list-form")
+  const newListTitle = document.getElementById("new-list-title")
+  const appContent = document.getElementById('app-content')
 
-  const app = new TaskLister();
-});
+  createListForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const listText = newListTitle.value
+    const newList = document.createElement('form')
+    const newListObj = new List(listText)
+    newList.innerText = newListObj.title
+    const newTask = document.createElement('div')
+    newTask.innerText = listText
+    const btn = document.createElement('button')
+    btn.innerText = 'Delete me!'
+    newTask.appendChild(btn)
+    const taskList = document.getElementById('lists')
+    taskList.appendChild(newList)
+    appContent.appendChild(newList)
+    event.target.reset()
+
+
+  })
+
+}); //end of function//
+
+let store = {lists: [],tasks: []}
+
+//
+// document.addEventListener(“DOMContentLoaded”, () => {
+//  const taskList = new TaskList();
+//  const TaskDescription = document.getElementById(‘new-task-description’);
+//  const createTaskForm = document.getElementById(‘create-task-form’);
+//  const tasks = document.getElementById(‘tasks’);
+//
+//  // createTaskForm.addEventListener(‘submit’, function(event) {
+//  //   event.preventDefault();
+//  //   const taskValue = TaskDescription.value
+//    const newTask = document.createElement(‘li’);
+//    const btn = document.createElement(“button”)
+//
+//    newTask.innerText = taskValue
+//    newTask.appendChild(btn)
+//    tasks.appendChild(newTask)
+//    event.target.reset()
+//
+//    btn.innerText = ‘X’
+//
+//    btn.addEventListener(‘click’, (event) => {
+//      event.target.parentNode.remove()
+//    })
+//  })
+//
+// }); //end of class
+//
+//
+//
--- src/task.js
+++ src/task.js
@@ -1,3 +1,12 @@
+let taskId = 0
+
 class Task {
   // your code here
+  constructor(listId, description, priority) {
+    this.id = ++taskId
+    this.listId = listId
+    this.description = description
+    this.priority = priority
+    store.tasks.push(this)
+  }
 }
